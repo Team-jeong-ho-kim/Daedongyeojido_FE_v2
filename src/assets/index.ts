40026354b1f4256b792b79a7de09aa7511b506d5
C4Z1N <leeyhdbajo@gmail.com>
2024-03-18 13:12:30 +0900

    head

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as LeftArrowBold } from "./img/LeftArrowBold.png";
+export { default as RightArrowBold } from "./img/RightArrowBold.png";
+export { default as DAELogo } from "./img/DAEDAE.png";
+export { default as DAEDAE } from "./img/daedaedae.png";
