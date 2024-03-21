5c66a5beaecc6d6bb1b8d34f2a044ed21c1be52e
은지 <wej0513@dsm.hs.kr>
2024-03-21 19:27:19 +0900

    test

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle.style";
+import { CheckClubPage } from "./page/club/CheckClubPage";
 
 function MainRouter() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/CheckClub" element={<CheckClubPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
