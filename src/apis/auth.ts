8c8e67f0d7d5fbe62d02033cfa68c12a1f52b51f
dutexion <dutexion@gmail.com>
2024-03-22 22:52:53 +0900

    ♻️ :: 명세 변경

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -2,6 +2,8 @@ import { LoginType } from "../types/type";
 import { instance } from "./axios";
 
 export const login = async (data: LoginType) => {
+  console.log(data);
+
   return await instance.post("/auth/login", data);
 };
 
