9893181ca552230cee1b929579926c04df003e04
dutexion <dutexion@gmail.com>
2024-04-02 19:31:04 +0900

    로긴 모달 스타일 변경

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -7,12 +7,15 @@ import { Cookie } from "../../utils/cookie";
 const Login = ({ onLoginToggle }: { onLoginToggle: () => void }) => {
   const [ID, setID] = useState("");
   const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState<boolean>(false);
 
   const handleClose = () => {
     onLoginToggle();
   };
 
-  const handleLogin = () => {
+  const handleLogin = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+
     login({
       account_id: ID,
       password: password,
@@ -21,35 +24,43 @@ const Login = ({ onLoginToggle }: { onLoginToggle: () => void }) => {
         Cookie.set("accessToken", res.data.accessToken);
         Cookie.set("refreshToken", res.data.refreshToken);
         Cookie.set("part", res.data.part);
-        handleClose();
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(() => {
+        alert("아이디를 확인해주세요");
+        setIsError(true);
       });
   };
 
   return (
     <>
-      <Container onClick={handleClose}></Container>
-      <LoginBox>
-        <Daedae>대동여지도</Daedae>
-        <LoginInput
-          placeholder="아이디"
-          value={ID}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setID(e.target.value)
-          }
-        />
-        <LoginInput
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-        <LoginButton onClick={handleLogin}>로그인</LoginButton>
-      </LoginBox>
+      <Container onClick={handleClose}>
+        <LoginBox onClick={(e) => e.stopPropagation()}>
+          <Daedae>대동여지도</Daedae>
+          <LoginInput
+            placeholder="아이디"
+            value={ID}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setID(e.target.value)
+            }
+          />
+          <div>
+            <LoginInput
+              type="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+            />
+            {isError && <Error>비밀번호를 확인해주세요</Error>}
+          </div>
+          <LoginButton
+            onClick={handleLogin}
+            canSubmit={Boolean(ID && password)}>
+            로그인
+          </LoginButton>
+        </LoginBox>
+      </Container>
     </>
   );
 };
@@ -79,21 +90,26 @@ const Container = styled.div`
   z-index: 500;
 `;
 
+const Error = styled.div`
+  height: 0px;
+  color: #f5290a;
+  font-size: 12px;
+  margin-left: 4px;
+`;
+
 const LoginBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 18.75px;
+  gap: 12px;
   padding-top: 0.75px;
-  position: fixed;
   top: calc(50% - 206.25px);
   left: calc(50% - 288px);
-  width: 576px;
-  height: 412.5px;
-  border-radius: 20px;
+  width: 360px;
+  height: 432px;
+  border-radius: 4px;
   background-color: #fff;
-  box-shadow: 0 10px 40px 0 rgba(0, 0, 0, 0.5);
   z-index: 501;
   animation: ${fadeIn} 0.5s;
 `;
@@ -101,22 +117,23 @@ const LoginBox = styled.div`
 const Daedae = styled.p`
   color: #000;
   font-family: "Spoqa Han Sans Neo";
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 700;
-  line-height: 20px;
+  margin-bottom: 12px;
 `;
 
-const LoginButton = styled.button`
-  width: 480px;
-  height: 62.5px;
-  background-color: #333b3d;
-  border-radius: 10px;
+const LoginButton = styled.button<{ canSubmit: boolean }>`
+  width: 312px;
+  height: 48px;
+  transition: 0.14s linear;
+  background-color: ${({ canSubmit }) => (canSubmit ? "#FF8086" : "#797d81")};
+  border-radius: 4px;
   color: #fff;
   font-family: "Spoqa Han Sans Neo";
-  font-size: 24px;
+  font-size: 14px;
   font-weight: 700;
-  line-height: 24px;
   cursor: pointer;
+  margin-top: 28px;
 `;
 
 export default Login;
